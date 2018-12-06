a0c7d9e64efb68fe0f9137c9e3a2003fb68adffb
Kennieslink <43042804+Kennieslink@users.noreply.github.com>
2018-12-06 18:41:01 +0100

    Add files via upload

---
+++ FizzBuzz Task/fizzbuzz.js
@@ -0,0 +1,10 @@
+
+for (let x = 0; x <= 100; x++) {
+    if (x % 15 == 0){
+      console.log(x + " FizzBuzz");
+    }else if(x % 3 == 0) {
+    console.log(x + " Fizz");
+    } else{
+    console.log(x + " Buzz")
+    }
+  } 
